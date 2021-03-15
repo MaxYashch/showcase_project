630bda384a37c7ed1413a2335afca729aef57f96
MaxYashch <maxyashch@gmail.com>
2021-03-15 20:37:01 +0300

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ function Header() {
     return (
         <nav className="deep-orange accent-3">
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo">
+                <a href="/" className="brand-logo">
                     React Shop
                 </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    {/* <li>
-                        <a href="#">Repo</a>
-                    </li> */}
+                    <li>
+                        <a
+                            href="https://github.com/MaxYashch/showcase_project"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Repo
+                        </a>
+                    </li>
                 </ul>
             </div>
         </nav>
